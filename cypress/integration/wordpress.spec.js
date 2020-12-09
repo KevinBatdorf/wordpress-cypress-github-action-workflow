d0095e55c68377c7317844b2d1c69f0c82fc4b3f
Kevin Batdorf <kbat82@gmail.com>
2020-12-09 18:54:05 +0700

    wip

--- cypress/integration/wordpress.spec.js
+++ cypress/integration/wordpress.spec.js
@@ -1,4 +1,4 @@
 it('should be able to login automatically and redirect to dashboard', () => {
-    cy.visit('/cypress')
+    cy.visit('http://localhost:8080/cypress')
     cy.url().should('contain', 'wp-admin')
 })
