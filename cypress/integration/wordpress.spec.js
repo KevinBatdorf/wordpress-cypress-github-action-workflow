6ac3472a683fea3dcf9570feaae9c16dc18259e4
Kevin Batdorf <kbat82@gmail.com>
2020-12-09 19:06:20 +0700

    wip

--- cypress/integration/wordpress.spec.js
+++ cypress/integration/wordpress.spec.js
@@ -1,4 +1,14 @@
 it('should be able to login automatically and redirect to dashboard', () => {
-    cy.visit('http://localhost:8080/cypress')
+    cy.visit('http://localhost:8080/wp-login.php')
+    // user login
+    cy.get('#user_login').type('wordpress', { delay: 0 });
+
+    // user pass
+    cy.get('#user_pass').type('wordpress', { delay: 0 });
+
+    // wp submit
+    cy.get('#wp-submit').click();
+
     cy.url().should('contain', 'wp-admin')
+    cy.should('contain.text', 'dashboard')
 })
